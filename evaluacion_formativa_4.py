e1b24bab3f70705924bfbbcbf94e78dd890279d9
Kevin Manuel <kevin.matu@gmail.com>
2025-10-29 17:30:33 -0300

    1.1

---
+++ evaluacion_formativa_4.py
@@ -0,0 +1,18 @@
+# Versión Corregida
+turistas = {
+    "001": ["John Doe", "Estados Unidos", "12-01-2024"],
+    "002": ["Emily Smith", "Estados Unidos", "23-03-2024"],
+    "012": ["Julian Martinez", "Argentina", "19-09-2023"],
+    "014": ["Agustin Morales", "Argentina", "28-03-2024"],
+    "005": ["Carlos Garcia", "Mexico", "10-05-2024"],
+    "006": ["Maria Lopez", "Mexico", "08-12-2023"],
+    "007": ["Joao Silva", "Brasil", "20-06-2024"],
+    "003": ["Michael Brown", "Estados Unidos", "05-07-2023"],
+    "004": ["Jessica Davis", "Estados Unidos", "15-11-2024"],  # <-- Corregido con una COMA
+    "008": ["Ana Santos", "Brasil", "03-10-2023"],
+    "010": ["Martin Fernandez", "Argentina", "13-02-2023"],
+    "011": ["Sofia Gomez", "Argentina", "07-04-2024"],
+}
+
+while True:
+    print('***MENU PRINCIPAL***')
